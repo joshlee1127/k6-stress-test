a260b8a7f28312666551bfcc7862e730ee1b9c4a
joshlee <joshlee@gamania.com>
2024-04-18 16:06:39 +0800

    k6: 07

--- v3/scripts/stress-testing.js
+++ v3/scripts/stress-testing.js
@@ -36,11 +36,11 @@ export const options = {
     scenarios: {
         constant_load: {
             executor: "ramping-arrival-rate",
-            startRate: 1, // 开始速率
+            startRate: 20, // 开始速率
             timeUnit: "1s", // 时间单位为秒
-            preAllocatedVUs: 10, // 预分配虚拟用户数
+            preAllocatedVUs: 50, // 预分配虚拟用户数
             stages: [
-                { duration: "120s", target: 50 }, // 第一个阶段持续 10 秒，目标 TPS 为 5
+                { duration: "600s", target: 50 }, // 第一个阶段持续 10 秒，目标 TPS 为 5
                 // { duration: "10s", target: 250 }, // 第二个阶段持续 10 秒，目标 TPS 为 10
                 // { duration: "10s", target: 300 }, // 第三个阶段持续 10 秒，目标 TPS 为 15
             ],
